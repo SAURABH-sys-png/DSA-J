5ff04819e8aff22e615da84552d9ef7b0e7b744d
realincor <saurabhrathee95@gmail.com>
2026-03-09 17:47:23 +0530

    09-03-2026

---
+++ leetcodeprobs/plusone.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std; 
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
+
+int main() {
+    vector<int> name;
+    for (int i = 0; i < 5; i++)
+    {
+        name.push_back(i+9);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << name[i];
+
+    }
+    cout << endl;
+    vector<int> plusonetwo;
+    
+    Solution obj;
+
+    plusonetwo = obj.plusOne(name);
+    cout <<"Here is " << endl;
+    for (int i = 0; i < 5; i++)//size issues
+    {
+        
+        cout << name[i];
+
+    }
+    return 0;
+}
