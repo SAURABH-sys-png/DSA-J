81565ade982aff67c9c2c6cb0443b4b4ce797131
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-04-15 23:13:50 +0530

    consistency is the key

--- 15aprlc.cpp
+++ 15aprlc.cpp
@@ -5,7 +5,40 @@
 
 using namespace std;
 
+class Solution {
+public:
+
+    bool hasZero(int n){
+
+        while(n != 0){
+            if(n%10 == 0){
+                return false;
+            }
+            n = n/10;
+
+        }
+        return true;
+    }
+    vector<int> getNoZeroIntegers(int n) {
+
+        vector<int> res;
+        int a = 1;
+        int b = n-1;
+        for(int i = 1;i<n;i++){
+            a = i;
+            b = n-a;
+            if(hasZero(a) && hasZero(b)){
+                res.push_back(a);
+                res.push_back(b);
+
+                return res;
+            }
+        }
+        return res;
+    }
+};
+
 int main (){
 
-    
+
 }
