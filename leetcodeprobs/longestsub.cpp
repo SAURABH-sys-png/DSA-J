8c132c8081099952e3797f2267cee6109d2e6118
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-04-30 13:14:24 +0530

    updated

--- leetcodeprobs/longestsub.cpp
+++ leetcodeprobs/longestsub.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <unordered_set>
+#include <vector>
 #include <string>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
@@ -8,22 +9,42 @@ using namespace std;
 class Solution
 {
 public:
+    int indexof(vector<char> sub, char n)
+    {
+        int res = -1;
+
+        for (int i = 0; i < sub.size(); i++)
+        {
+            if (sub[i] == n)
+            {
+                return i;
+            }
+        }
+
+        return res;
+    }
+
     int lengthOfLongestSubstring(string s)
     {
+
         int res = 0;
-        unordered_set<char> charSet;
-        int left = 0;
+        vector<char> substring;
 
         for (int i = 0; i < s.size(); i++)
         {
-            // If duplicate found, remove characters from left until duplicate is gone
-            while (charSet.find(s[i]) != charSet.end())
+            if (indexof(substring, s[i]) >= 0)
             {
-                charSet.erase(s[left]);
-                left++;
+                substring.erase(substring.begin(), substring.begin() + indexof(substring, s[i]) + 1);
+                substring.push_back(s[i]);
+                // kush to bhi karna hai
             }
-            charSet.insert(s[i]);
-            res = max(res, (int)charSet.size());
+            else
+            {
+                substring.push_back(s[i]);
+            }
+            int count = substring.size();
+
+            res = max(res, count);
         }
 
         return res;
