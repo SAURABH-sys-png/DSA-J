e9c91ad1317c636b5021cd4551a9ae797a48ea73
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-04-30 23:12:50 +0530

    made things cleaner

---
+++ leetcodeprobs/maxipath.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        
+    }
+};
+
+int main(){
+
+    return 0;
+}
