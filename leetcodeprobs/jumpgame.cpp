e1a55846901081e3fa915d436bab7edb0a6f8456
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-05-07 23:07:54 +0530

    forget to sv

--- leetcodeprobs/jumpgame.cpp
+++ leetcodeprobs/jumpgame.cpp
@@ -7,49 +7,55 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> maxValue(vector<int>& nums) {
+    vector<int> maxValue(vector<int> &nums)
+    {
         int n = nums.size();
 
         vector<int> maxLeft;
         vector<int> ans(n);
 
-       
-
         int maxtil = -1;
-        for(int i = 0;i < n ;i++){
-            if(maxtil > nums[i]){
+        for (int i = 0; i < n; i++)
+        {
+            if (maxtil > nums[i])
+            {
                 maxLeft.push_back(maxtil);
             }
-            else if(nums[i] > maxtil){
+            else if (nums[i] > maxtil)
+            {
                 maxLeft.push_back(nums[i]);
                 maxtil = nums[i];
             }
-            else{
+            else
+            {
                 maxLeft.push_back(nums[i]);
             }
         }
 
-
         int minimu = INT_MAX;
 
-        for(int k = n-1;k>=0;k--){
-            if(maxLeft[k] > minimu){
-                ans[k] = ans[k+1];
+        for (int k = n - 1; k >= 0; k--)
+        {
+            if (maxLeft[k] > minimu)
+            {
+                ans[k] = ans[k + 1];
             }
-            else{
-                ans[k] =  maxLeft[k];
+            else
+            {
+                ans[k] = maxLeft[k];
             }
 
-            minimu = min(minimu,nums[k]);
+            minimu = min(minimu, nums[k]);
         }
         return ans;
     }
 };
 
-
-int main(){
+int main()
+{
 
     return 0;
 }
