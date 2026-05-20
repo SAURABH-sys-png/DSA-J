1976679be929ff1f22abb2a7409a17ea22160055
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-05-20 15:40:36 +0530

    Prefix freq prob

--- leetcodeprobs/lc_prefix_freq.cpp
+++ leetcodeprobs/lc_prefix_freq.cpp
@@ -2,37 +2,49 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+    vector<int> findThePrefixCommonArray(vector<int> &A, vector<int> &B)
+    {
         vector<int> res;
         int n = A.size();
         int common = 0;
-        unordered_map<int,int> freq;
-        for (int i = 0; i < n; i++) {
-            freq[A[i]]++;
-            freq[B[i]]++;
-            if (freq[A[i]] == 2) common++;
-            if (freq[B[i]] == 2) common++;
+        unordered_map<int, int> freq;
+        for (int i = 0; i < n; i++)
+        {
+            if (A[i] == B[i])
+                common++;
+            else
+            {
+                freq[A[i]]++;
+                freq[B[i]]++;
+            }
+
+            if (freq[A[i]] == 2)
+                common++;
+            if (freq[B[i]] == 2)
+                common++;
             res.push_back(common);
         }
         return res;
     }
 };
 
-
-void solve() {
-    
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(NULL);
 
     int t = 1;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
 
