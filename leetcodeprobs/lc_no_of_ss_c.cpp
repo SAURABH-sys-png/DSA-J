b4a8350b0d4f44c8e7a2189beea2a9f6b01d16f6
SAURABH-sys-png <saurabhrathee95@gmail.com>
2026-07-01 00:21:55 +0530

    Jul 1

--- leetcodeprobs/lc_no_of_ss_c.cpp
+++ leetcodeprobs/lc_no_of_ss_c.cpp
@@ -1,17 +1,90 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(NULL);
+#define fastio               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(NULL);
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numberOfSubstrings(string s) {
-        
+    void updt(char ch, bool &a, bool &b, bool &c)
+    {
+        if (ch == 'a')
+        {
+            a = true;
+        }
+        else if (ch == 'b')
+        {
+            b = true;
+        }
+        else
+        {
+            c = true;
+        }
+    }
+    // abcabc
+    int numberOfSubstringssr(string s)
+    {
+        int n = s.size();
+
+        bool a = false;
+        bool b = false;
+        bool c = false;
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            a = false;
+            b = false;
+            c = false;
+
+            updt(s[i], a, b, c);
+
+            for (int j = i + 1; j < n; j++)
+            {
+                updt(s[j], a, b, c);
+                if (a && b && c)
+                {
+                    cnt = cnt + (n - j);
+                    break;
+                }
+            }
+        }
+        // abc,abca,abcab,abcabc,bca,bcabc,cab,cabc,abc
+        return cnt;
+    }
+
+    int numberOfSubstrings(string s)
+    {
+
+        int last_a = -1, last_b = -1, last_c = -1;
+        int cnt = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'a')
+                last_a = i;
+            else if (s[i] == 'b')
+                last_b = i;
+            else if (s[i] == 'c')
+                last_c = i;
+
+            if (last_a != -1 && last_b != -1 && last_c != -1)
+            {
+
+                cnt += (1 + min({last_a, last_b, last_c}));
+            }
+        }
+        return cnt;
     }
 };
 
-
-int main() {
+int main()
+{
 
     string s = "abcabc";
     Solution sol;
