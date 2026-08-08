6be8916439d1f94b6b4a4fb3e57f10575b7161db
realincor <saurabhrathee95@gmail.com>
2026-08-08 02:11:52 -0700

    Aug 8

---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+void print(int n){
+	for(int i = 0;i<n;i++){
+		cout << i  << endl;
+	}
+}
+int main(){
+	int n;
+	cin >> n;
+	print(n);
+	return 0;
+}
